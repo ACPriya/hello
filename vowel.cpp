15b26cdb4f1dd71ccdc59eb953484d32e6528bc3
ACPriya <34775420+ACPriya@users.noreply.github.com>
2018-01-05 16:05:19 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int i;
+    char a;
+    cin>>a;
+    
+        if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
+        cout<<"Vowel"<<endl;
+        else
+        cout<<"not a vowel";
+       return 0;
+}
+
