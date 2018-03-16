7641ea249401c711699b7235eb98fbbd70a1f1ef
ACPriya <34775420+ACPriya@users.noreply.github.com>
2018-03-16 15:29:17 +0530

    Create Repeat.java

---
+++ Repeat.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Repeat
+{    
+    static void mostrepeat(String s)
+    {
+    	char[] str=s.toCharArray();    	    	  
+    	int leng= s.length()-1;     	
+    	int prev=0;char c=' ';
+    	while(leng>=0)
+    	{    		
+    		int temp=0;
+    		for(int i=0;i<=leng;i++)
+    		{
+    			if(str[leng]==str[i])
+    				temp++;    				
+    		}
+    		if(temp>prev)
+    		{
+    			prev=temp;
+    			c=str[leng];
+    		}
+    		leng--;
+    	}
+    	System.out.println(c);
+    }
+    public static void main (String[] args) 
+    {
+    	Scanner sn = new Scanner(System.in);
+    	String s = sn.nextLine();
+    	mostrepeat(s);
+    }
+}
